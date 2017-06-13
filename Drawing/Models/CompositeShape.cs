0c15d86f57ba511aac50190444245a6a3959b94a
Ying Du <ying.du@live.com>
2017-06-13 19:17:17 +0800

    优化CompositeShape类

--- Drawing/Models/CompositeShape.cs
+++ Drawing/Models/CompositeShape.cs
@@ -4,34 +4,18 @@ using System.Windows.Media;
 
 namespace Drawing.Models
 {
-    class CompositeShape : ShapeBase
+    class CompositeShape : IDrawable
     {
         List<ShapeBase> compositeshape = new List<ShapeBase>();
-        public override double Area => throw new NotImplementedException();
         public GeometryGroup GeometryGroup { get; private set; } = new GeometryGroup();
 
-        public override ShapeBase Clone()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Draw()
+        public void Draw()
         {
             foreach (ShapeBase shape in compositeshape)
             {
                 shape.Draw();
                 shape.Instance.Fill = Brushes.Blue;
             }
-        }
-
-        public override double GetPerimeter()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void Zoom(double multiple)
-        {
-            throw new NotImplementedException();
         }
 
         public void Add(ShapeBase shape)
